3e059ae98bf5e701593f9027ffd2dec8e6abbc3c
Masahiro Sakai <masahiro.sakai@gmail.com>
2025-04-20 21:14:49 +0900

    add table of instances on README.md

--- check.rb
+++ check.rb
@@ -4,17 +4,40 @@ require 'set'
 instances_dir = Pathname.new("instances")
 qplib_instances_dir = Pathname.new("qplib/html/qplib")
 
+header = ["name", "problem type", "#variable", "#constraint", "#equal", "intsize", "product", "sizeproduct"]
+rows = []
 types = Set.new
 
-instances_dir.glob("*.opb").sort.each{ |path_opb|
+instances_dir.glob("*.opb").sort_by { |path_opb|
+  name = path_opb.basename(".opb").to_s.sub(/^QPLIB_/, '').to_i
+}.each{ |path_opb|
   name = path_opb.basename(".opb").to_s
+  row = [name]
+ 
   File.open(qplib_instances_dir / (name + ".qplib")) { | f|
     f.gets
-    types << f.gets.strip
+    type = f.gets.strip
+    types << type
+    row << type
   }
+  
+  File.open(path_opb) { |f|
+    ws = f.gets.sub(/^* /, '').split
+    1.step(ws.size, 2) { |i|
+      row << ws[i]
+    }
+  }
+
+  rows << row
 }
 
 puts "converted problem types: #{types.to_a.join(", ")}"
+puts ""
+puts("|" + header.join("|") + "|")
+puts("|" + ["-", "-", "-:", "-:", "-:", "-:", "-:", "-:"].join("|") + "|")
+rows.each { |row|
+  puts("|" + row.join("|") + "|")
+}
 
 qplib_instances_dir.glob("*.qplib").each{ |path_qplib|
   name = path_qplib.basename(".qplib").to_s
