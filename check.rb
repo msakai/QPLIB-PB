99036e2b893c2d398ffa77b9e01a29ea383a5ece
Masahiro Sakai <masahiro.sakai@gmail.com>
2025-04-20 22:03:13 +0900

    improve instance table on README.md

--- check.rb
+++ check.rb
@@ -1,20 +1,37 @@
+require 'json'
 require 'pathname'
 require 'set'
 
 instances_dir = Pathname.new("instances")
 qplib_instances_dir = Pathname.new("qplib/html/qplib")
 
-header = ["name", "problem type", "#variable", "#constraint", "#equal", "intsize", "product", "sizeproduct"]
+header = ["name", "QPLIB link", "problem type in QPLIB", "#variable", "#constraint", "#equal", "intsize", "product", "sizeproduct", "objective function scale factor"]
 rows = []
 types = Set.new
+
+def find_objective_function_scale_factor(json)
+  if json.key? "objective_function_scale_factor"
+    return json["objective_function_scale_factor"]
+  end
+
+  if json["type"] == "ComposedTransformer"
+    return find_objective_function_scale_factor(json["first"]) ||
+           find_objective_function_scale_factor(json["second"])
+  end
+
+  nil
+end
 
 instances_dir.glob("*.opb").sort_by { |path_opb|
   name = path_opb.basename(".opb").to_s.sub(/^QPLIB_/, '').to_i
 }.each{ |path_opb|
   name = path_opb.basename(".opb").to_s
-  row = [name]
+  row = [
+    "[#{name}](#{path_opb.to_s})",
+    "[link](https://qplib.zib.de/#{name}.html)"
+  ]
  
-  File.open(qplib_instances_dir / (name + ".qplib")) { | f|
+  File.open(qplib_instances_dir / (name + ".qplib")) { |f|
     f.gets
     type = f.gets.strip
     types << type
@@ -28,13 +45,21 @@ instances_dir.glob("*.opb").sort_by { |path_opb|
     }
   }
 
+  json = JSON.parse((instances_dir / "#{name}.json").read())
+  objective_function_scale_factor = find_objective_function_scale_factor(json)
+  if objective_function_scale_factor["denominator"] == 1
+    row << format("%g", objective_function_scale_factor["numerator"])
+  else
+    row << "#{objective_function_scale_factor["numerator"]}/#{objective_function_scale_factor["denominator"]}"
+  end
+
   rows << row
 }
 
 puts "converted problem types: #{types.to_a.join(", ")}"
 puts ""
 puts("|" + header.join("|") + "|")
-puts("|" + ["-", "-", "-:", "-:", "-:", "-:", "-:", "-:"].join("|") + "|")
+puts("|" + ["-", "-", "-", "-:", "-:", "-:", "-:", "-:", "-:", "-:"].join("|") + "|")
 rows.each { |row|
   puts("|" + row.join("|") + "|")
 }
